b556f4d25b9b90dc3c793373a1a54c331d90b5f5
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-01-12 16:36:55 +0200

    Add submissions page structure

--- src/components/doctor/SupervisorSubmissions.tsx
+++ src/components/doctor/SupervisorSubmissions.tsx
@@ -1,5 +1,45 @@
+import { Container, Grid, Paper, Typography } from "@mui/material";
+import Abstracts from "./Abstracts";
+
 const SupervisorSubmissions = () => {
-  return <div>Supervisor Submissions</div>;
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4 }}>
+      <Paper
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          my: 4,
+        }}
+      >
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <Typography
+              component="h2"
+              variant="h4"
+              color="primary"
+              gutterBottom
+            >
+              Abstracts Submissions
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{ display: "flex", alignItems: "center" }}
+            gap={2}
+          >
+            <Typography variant="h6">
+              Here you can view projects abstracts.
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Abstracts />
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
+  );
 };
 
 export default SupervisorSubmissions;
